760e8de6ac44edda782bd117c86433603e14d963
cadelucci8 <cade.angelucci@gmail.com>
2024-11-26 17:30:59 -0500

    Updated Javascript

--- js/main.js
+++ js/main.js
@@ -2,24 +2,40 @@
 const WORD_LIST = ['STARSHIP', 'ASTEROID', 'ORBITAL'];
 
 /*----- state variables -----*/
-let word, guessesRemaining, winner;
+let Hiddenword, guessesRemaining, guessedLetters, incorrectLetters;
 
 /*----- cached elements  -----*/
-
-
+const message = document.querySelector('h2');
+const playAgainButton = document.querySelector('#reset-button');
+guessesRemaining = document.querySelector('p-gusses');
 /*----- event listeners -----*/
-
-
+playAgainButton.addEventListener('click', init);
+document.getElementById('board').addEventListener('click', handleGuess);
 /*----- functions -----*/
-initialize();
+init();
 
-function initialize() {
-    word = WORD_LIST[Math.floor(Math.random() * WORD_LIST.length)]
+function init() {
+    Hiddenword = WORD_LIST[Math.floor(Math.random() * WORD_LIST.length)]
     guessesRemaining = 12;
-    winner = null;
+    guessedLetters = [];
+    incorrectLetters = [];
     render();
 }
 
 function render() {
-    
+    handleGuess();
+    renderButtons();
+    renderMessage();
+}
+
+function handleGuess(evt) {
+    return guessesRemaining -= 1;
+}
+
+function renderButtons() {
+
+}
+
+function renderMessage() {
+
 }
